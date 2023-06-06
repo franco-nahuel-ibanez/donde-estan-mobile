ad4a34ebd190afadbe497d2dbc797466861963be
franco-nahuel-ibanez <fibanez@espinlabs.com.ar>
2023-06-05 21:53:51 -0300

    formulario con date picker. sin imagen

--- src/screens/register/Register.tsx
+++ src/screens/register/Register.tsx
@@ -1,14 +1,15 @@
-
 import React from 'react'
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native'
 import { Header } from '../../components/Header'
 import { useFormik } from 'formik'
 import { register } from './schemas/Index';
-import { Ionicons } from '@expo/vector-icons';
-import { TextInput as Custom } from './components/TextInput';
+import RNDateTimePicker, { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
+
 
 
 export default function Register() {
+  const [date, setDate] = React.useState<Date>(new Date());
+  const [showDatePicker, setShowDatePicker] = React.useState(false);
 
   const {
     errors,
@@ -17,12 +18,13 @@ export default function Register() {
     handleSubmit,
     handleBlur,
     touched,
+    setFieldValue,
   } = useFormik({
     initialValues: {
       name: '',
       lastName: '',
       age: '',
-      dateOfDisappearance: '',
+      dateOfDisappearance: date,
       placeOfDisappearance: '',
       description: '',
       image: '',
@@ -32,11 +34,29 @@ export default function Register() {
     onSubmit: (values) => {
       console.log("")
     },
-    validationSchema: register,
+    validationSchema: register, 
   });
+
+  const handleDateChange = (event: any, selectedDate: any) => {
+    setShowDatePicker(false);
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+    setFieldValue('dateOfDisappearance', currentDate);
+  };
 
   return (
     <ScrollView>
+      {
+        showDatePicker && (
+          <RNDateTimePicker
+            mode='date'
+            onTouchCancel={() => setShowDatePicker(false)}
+            value={date}
+            onChange={handleDateChange}
+          />
+        )
+      }
+      
       <Header>
         <Image
           source={require('../../../assets/logo.png')}
@@ -106,19 +126,117 @@ export default function Register() {
         </View>
 
         <View style={styles.inputContainer}>
-          <Text style={styles.label}>Fecha de desaparición</Text>        
+          <Text style={styles.label}>Fecha de desaparición</Text>
+          <TouchableOpacity
+            onPress={() => setShowDatePicker(true)}
+            activeOpacity={0.8}
+          >
+            <TextInput
+              placeholder="Fecha de desaparición"
+              style={styles.input}
+              autoComplete='off'
+              autoCorrect={false}
+              value={values.dateOfDisappearance.getDate() + '/' + (values.dateOfDisappearance.getMonth() + 1) + '/' + values.dateOfDisappearance.getFullYear()}
+              onChangeText={handleChange('dateOfDisappearance')}
+              onBlur={handleBlur('dateOfDisappearance')}
+              editable={false}
+            /> 
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Lugar de desaparición</Text>        
           <TextInput
-            placeholder="Fecha de desaparición"
+            placeholder="Lugar de desaparición"
             style={styles.input}
             autoComplete='off'
             autoCorrect={false}
-            value={values.dateOfDisappearance}
-            onChangeText={handleChange('dateOfDisappearance')}
-            onBlur={handleBlur('dateOfDisappearance')}
+            value={values.placeOfDisappearance}
+            onChangeText={handleChange('placeOfDisappearance')}
+            onBlur={handleBlur('placeOfDisappearance')}
           />
           {
-            (touched.dateOfDisappearance && errors.dateOfDisappearance) && (
-              <Text style={styles.error}>{errors.dateOfDisappearance}</Text>
+            (touched.placeOfDisappearance && errors.placeOfDisappearance) && (
+              <Text style={styles.error}>{errors.placeOfDisappearance}</Text>
+            )
+          }
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Teléfono</Text>        
+          <TextInput
+            keyboardType='numeric'
+            placeholder="Numero de Teléfono"
+            style={styles.input}
+            autoComplete='off'
+            autoCorrect={false}
+            value={values.phone}
+            onChangeText={handleChange('phone')}
+            onBlur={handleBlur('phone')}
+          />
+          {
+            (touched.phone && errors.phone) && (
+              <Text style={styles.error}>{errors.phone}</Text>
+            )
+          }
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Correo</Text>        
+          <TextInput
+            keyboardType='email-address'
+            placeholder="Email (opcional)"
+            style={styles.input}
+            autoComplete='off'
+            autoCorrect={false}
+            value={values.email}
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+          />
+          {
+            (touched.email && errors.email) && (
+              <Text style={styles.error}>{errors.email}</Text>
+            )
+          }
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Imagen</Text>        
+          <TextInput
+            placeholder="Imagen"
+            style={styles.input}
+            autoComplete='off'
+            autoCorrect={false}
+            value={values.image}
+            onChangeText={handleChange('image')}
+            onBlur={handleBlur('image')}
+          />
+          {
+            (touched.image && errors.image) && (
+              <Text style={styles.error}>{errors.image}</Text>
+            )
+          }
+        </View>
+
+
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Descripción</Text>        
+          <TextInput
+            multiline={true}
+            numberOfLines={4}
+            maxLength={500}
+            placeholder="Agregar Descripción (opcional)"
+            style={styles.textArea}
+            autoComplete='off'
+            autoCorrect={false}
+            value={values.description}
+            onChangeText={handleChange('description')}
+            onBlur={handleBlur('description')}
+          />
+          {
+            (touched.description && errors.description) && (
+              <Text style={styles.error}>{errors.description}</Text>
             )
           }
         </View>
@@ -128,8 +246,9 @@ export default function Register() {
         <TouchableOpacity
           onPress={() => handleSubmit()}
           activeOpacity={0.8}
+          style={styles.button}
         >
-          <Text>Enviar</Text>
+          <Text style={styles.buttonText}>Enviar</Text>
         </TouchableOpacity>
 
 
@@ -173,9 +292,35 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginVertical: 5,
   },
+  textArea: {
+    height: 100,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    marginVertical: 5,
+    paddingVertical: 10,
+    textAlignVertical: 'top',
+  },
   error: {
     color: 'red',
     fontSize: 16,
     marginHorizontal: 10,
-  }
+  },
+  button: {
+    backgroundColor: '#f29d64',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginVertical: 5,
+    height: 50,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#fff',
+  },
+
 })
